558c67560113d9ed3494d34e4772b269febcf18f
Arpojan <arvauzanp@gmail.com>
2026-08-20 06:40:32 +0700

    Fix API ControlConfigResource not sending temp_max & humid_min threshold based on animal species

--- app/Http/Resources/ControlConfigResource.php
+++ app/Http/Resources/ControlConfigResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\Enclosure;
+use App\Services\AnimalKnowledgeBase;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -19,6 +20,13 @@ class ControlConfigResource extends JsonResource
         /** @var Enclosure $this->resource */
         $params = $this->resource->parameters;
 
+        $speciesKey = $this->resource->species_key ?? $this->resource->jenis_hewan;
+        $species = $speciesKey ? AnimalKnowledgeBase::getSpeciesByKey($speciesKey) : null;
+
+        $tMax = $species ? (float) $species['temperature']['temp_max'] : 31.0;
+        $rhMin = $species ? (float) $species['humidity']['humid_min'] : ($params ? (float) $params->humidity_min : 55.0);
+        $rhMax = $species ? (float) $species['humidity']['humid_max'] : ($params ? (float) $params->humidity_max : null);
+
         return [
             'enclosure_id'             => $this->resource->id,
             'enclosure_name'           => $this->resource->name,
@@ -27,9 +35,23 @@ class ControlConfigResource extends JsonResource
             'bottom_humidity'          => $params ? (float) $params->misting_bottom_threshold : null,
             'top_humidity'             => $params ? (float) $params->misting_top_threshold : null,
             'misting_duration_seconds' => $params ? (int) $params->misting_duration_seconds : null,
-            'humidity_min'             => $params ? (float) $params->humidity_min : null,
-            'humidity_max'             => $params ? (float) $params->humidity_max : null,
+            'humidity_min'             => $rhMin,
+            'humidity_max'             => $rhMax,
+            't_max'                    => $tMax,
+            'temp_max'                 => $tMax,
+            'temperature_max'          => $tMax,
             'updated_at'               => $params?->updated_at?->toIso8601String(),
+            
+            // ── Species thresholds dari AnimalKnowledgeBase ──────────────
+            'species_key'              => $speciesKey,
+            'species_name'             => $species['name'] ?? null,
+            'temp_min'                 => $species ? (float) $species['temperature']['temp_min'] : null,
+            'temp_ideal_min'           => $species ? (float) $species['temperature']['temp_ideal_min'] : null,
+            'temp_ideal_max'           => $species ? (float) $species['temperature']['temp_ideal_max'] : null,
+            'humid_min_critical'       => $species ? (float) $species['humidity']['humid_min'] : null,
+            'humid_ideal_min'          => $species ? (float) $species['humidity']['humid_ideal_min'] : null,
+            'humid_ideal_max'          => $species ? (float) $species['humidity']['humid_ideal_max'] : null,
+            'humid_max_critical'       => $species ? (float) $species['humidity']['humid_max'] : null,
         ];
     }
 }
